7773619e8cd4e3d29d73076b5b01bf4ad057eefb
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-04 01:32:36 +0530

    Add files via upload

---
+++ AddingNumAnonymous/AddingNumAnonymous/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingNumAnonymous
+{
+    
+    class Program
+    {
+        public delegate int sum(int i, int j);
+        static void Main(string[] args)
+        {
+            int result;
+            sum s = delegate (int i,int j)
+            {
+                return i + j;
+            };
+            result = s(10, 20);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ StreamDemoApp/StreamDemoApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamDemoApp
+{
+    class WriteAndReader
+
+    {
+        static string file = @"D:\UST\Stream\StreamTxt.txt";
+
+        static void Main(string[] args)
+        {
+            Write();
+        }
+        static void Write()
+        {
+            StreamWriter fileStream = new StreamWriter(file);
+            fileStream.WriteLine("What is this life, full of care");
+            fileStream.WriteLine("We have no time to stand and stare");
+            fileStream.Close();
+        }
+        static void Read()
+        {
+            StreamReader fileStream = new StreamReader(file);
+            string s = null;
+            while ((s = fileStream.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+            fileStream.Close();
+        }
+    }
+}
